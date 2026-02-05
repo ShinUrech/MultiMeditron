493eb9d364ead3683ae8ecd714f319973f8b86ec
fabnemEPFL <117652591+fabnemEPFL@users.noreply.github.com>
2026-02-05 11:54:52 +0100

    Revert "Experts code submission"

--- src/multimeditron/experts/evaluation_pipeline/disease_classification_pipeline/Benchmark.py
+++
@@ -1,9 +0,0 @@
-from abc import ABC, abstractmethod
-from transformers import VisionTextDualEncoderConfig, VisionTextDualEncoderModel
-
-class Benchmark(ABC):
-
-    #takes as input input the path of the evaluated model and returns the evaluation metrics which has to be maximized
-    @abstractmethod
-    def evaluate(self, model_path) -> float:
-        pass
